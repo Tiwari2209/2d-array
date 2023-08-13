096e0206c58fcd89fa0d3f4ca9e53fc6e888887f
Tiwari2209 <90821997+Tiwari2209@users.noreply.github.com>
2023-08-13 23:33:53 +0530

    Update diagonal.java

--- diagonal.java
+++ diagonal.java
@@ -50,6 +50,6 @@ public class diagonal{
     } 
     public static void main(String args[]){
         int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        diagonalSum(matrix);
+        System.out.println(diagonalSum(matrix));
     }
 }
